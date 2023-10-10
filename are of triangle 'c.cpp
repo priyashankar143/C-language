62b76a2a3befd2dbe104a24c11e32937519fdb3a
priyashankar143 <147472529+priyashankar143@users.noreply.github.com>
2023-10-10 11:24:13 +0530

    Add files via upload

---
+++ are of triangle 'c.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int l1=10,l2=25;
+	Float area;
+	printf("Enter of two numbers");
+	scanf("%d%d",&l1,&l2);
+	area=l1*l2/2;
+	printf("area=%f",area);
+	return 0;
+}
+
